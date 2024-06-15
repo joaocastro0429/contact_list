d42a979ee6937351012f3ca2a93c181eb79a231b
joaocastro <joaocastro0429@gmail.com>
2024-06-15 08:27:53 -0300

    project contact

---
+++ config/importmap.rb
@@ -0,0 +1,2 @@
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/umd/popper.min.js", preload: true
